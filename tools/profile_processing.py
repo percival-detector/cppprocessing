f139d732363ffc2cd5a9fce7776c9e1446de9242
qgu <qiushi.gu@diamond.ac.uk>
2015-08-10 23:42:51 +0100

    Added function to automatically format oprofile options string.

--- tools/profile_processing.py
+++ tools/profile_processing.py
@@ -2,6 +2,24 @@
 import re
 import subprocess
 
+class oprofile_events:
+    event_name = ''
+    unit_mask = ''
+    sample_rate = 10000
+    def __init__(self, event_name, unit_mask, sample_rate):
+	self.event_name = event_name
+	self.unit_mask = unit_mask
+	self.sample_rate = sample_rate
+    def getString(self):
+	return self.event_name + ':' +str(self.sample_rate)+':'+self.unit_mask+':1:1'
+
+def get_operf_option(list):
+    str = '-e '
+    for event in list:
+	str = str + event.getString()
+    	str = str + ','
+    return str[:-1]
+
 def run_the_function(print_result):
     #subprocess.call('LD_LIBRARY_PATH="/dls_sw/prod/tools/RHEL6-x86_64/hdf5/1-8-15/prefix/lib"', shell=True)
     #subprocess.call('HDF5_DISABLE_VERSION_CHECK="1"', shell=True)
@@ -9,31 +27,45 @@ def run_the_function(print_result):
     top_level_data_set_name= "KnifeQuadBPos1/"
     
     repeat = 500
-    width = 210
+    width = 1024
+    debug_version = './Debug/cppProcessing2.0 '
+    profile_version = './Profiling/cppProcessing2.0 '
+
     #program_to_execute = './Profiling/cppProcessing2.0 '#Use meaningful data to run # + str(width) + ' ' + str(repeat) 
-    program_to_execute = './Debug/cppProcessing2.0 ' + "0 "+ path_name + " " + top_level_data_set_name + " " + str(repeat)
-    
+#    program_to_execute = './Debug/cppProcessing2.0 ' + "0 "+ path_name + " " + top_level_data_set_name + " " + str(repeat)
+    cmdl_arg = '1 ' + str(width) + ' ' + str(repeat)    
+    program_to_execute = debug_version + cmdl_arg
+
     #events to monitor
     #clock cycle
     cpu_clock_sample_counts = 100000
     unit_mask = 0x00
     CPU_CLK_UNHALTED = 'CPU_CLK_UNHALTED:' + str(cpu_clock_sample_counts) + ':' + str(unit_mask) + ':1:1,'
     #cache miss
+    event1 = oprofile_events('CPU_CLK_UNHALTED','0x00',100000)
+    event2 = oprofile_events('INST_RETIRED','0x00',6000)
+    event3 = oprofile_events('mem_load_uops_llc_hit_retired','0x02',100000)
+    event4 = oprofile_events('mem_load_uops_llc_hit_retired','0x04',100000)
+    event5 = oprofile_events('mem_load_uops_retired','0x04',2000000)
+ 
+    list_of_events = [event1, event2, event3, event4, event5]
+    list_of_attributes = []
+#    mem_load_uops_llc_hit_retired_0x02_sample_counts = 100000                #100000 min
+#    unit_mask = 0x02
+#    mem_load_uops_llc_hit_retired_0x02 = 'mem_load_uops_llc_hit_retired:' + str(mem_load_uops_llc_hit_retired_0x02_sample_counts) + ':' + str(unit_mask) + ':1:1,'
     
-    mem_load_uops_llc_hit_retired_0x02_sample_counts = 100000                #100000 min
-    unit_mask = 0x02
-    mem_load_uops_llc_hit_retired_0x02 = 'mem_load_uops_llc_hit_retired:' + str(mem_load_uops_llc_hit_retired_0x02_sample_counts) + ':' + str(unit_mask) + ':1:1,'
+
+#    mem_load_uops_llc_hit_retired_0x04_sample_counts = 100000                #100000 min
+#    unit_mask = 0x04
+#    mem_load_uops_llc_hit_retired_0x04 = 'mem_load_uops_llc_hit_retired:' + str(mem_load_uops_llc_hit_retired_0x04_sample_counts) + ':' + str(unit_mask) + ':1:1,'
     
-    mem_load_uops_llc_hit_retired_0x04_sample_counts = 100000                #100000 min
-    unit_mask = 0x04
-    mem_load_uops_llc_hit_retired_0x04 = 'mem_load_uops_llc_hit_retired:' + str(mem_load_uops_llc_hit_retired_0x04_sample_counts) + ':' + str(unit_mask) + ':1:1,'
+#    mem_load_uops_retired_0x04_sample_counts = 2000000                #2000000 min
+#    unit_mask = 0x04
+#    mem_load_uops_retired_0x04 = 'mem_load_uops_retired:' + str(mem_load_uops_retired_0x04_sample_counts) + ':' + str(unit_mask) + ':1:1,'
     
-    mem_load_uops_retired_0x04_sample_counts = 2000000                #2000000 min
-    unit_mask = 0x04
-    mem_load_uops_retired_0x04 = 'mem_load_uops_retired:' + str(mem_load_uops_retired_0x04_sample_counts) + ':' + str(unit_mask) + ':1:1,'
-    
-    operf_events = '-e ' + mem_load_uops_llc_hit_retired_0x02 + mem_load_uops_llc_hit_retired_0x04 + mem_load_uops_retired_0x04
-    
+#    operf_events = '-e ' + mem_load_uops_llc_hit_retired_0x02 + mem_load_uops_llc_hit_retired_0x04 + mem_load_uops_retired_0x04
+ 
+    operf_events = get_operf_option(list_of_events)
     print 'operf ' + operf_events + ' '+ program_to_execute
     
     subprocess.call('(/usr/bin/time -v ' + program_to_execute + ') &> profile_report.txt', shell=True)
@@ -50,6 +82,10 @@ def run_the_function(print_result):
     percentage_time_CDS_correction = 0
     percentage_time_ADU_to_electron_correction = 0
     while s != '':
+	if 'Counted' in s:
+	    for event in list_of_events:
+		if event.event_name in s:
+  		    list_of_events_recorded.append(event)
         if 'Elapsed (wall clock) time ' in s:
             parsed = s.split(' ')
             time = parsed[len(parsed)-1]
@@ -72,8 +108,9 @@ def run_the_function(print_result):
             
         elif 'percival_ADC_decode' in s:
             delimited = s.split(' ')
-            parsed = [item for item in delimited if item != '']
+            parsed = [item for item in delimited if item != '']    
             percentage_time_ADC_decode = float(parsed[1])
+	    
         elif 'percival_CDS_correction'in s:
             delimited = s.split(' ')
             parsed = [item for item in delimited if item != '']
