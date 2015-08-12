c71fc605712c8f02d61e1953ad26547743bc446b
qgu <qiushi.gu@diamond.ac.uk>
2015-08-12 14:39:40 +0100

    Added previously not included files to git.

---
+++ Include/percival_load_calib_params.h
@@ -0,0 +1,17 @@
+/*
+ * percival_load_calib_params.h
+ *
+ *  Created on: 12 Aug 2015
+ *      Author: pqm78245
+ */
+
+#ifndef H5_SRC_PERCIVAL_LOAD_CALIB_PARAMS_H_
+#define H5_SRC_PERCIVAL_LOAD_CALIB_PARAMS_H_
+
+#include "percival_calib_params.h"
+#include "percival_global_params.h"
+
+void percival_load_calib_params(percival_calib_params & calib_params, percival_global_params & global_params);
+
+
+#endif /* H5_SRC_PERCIVAL_LOAD_CALIB_PARAMS_H_ */
