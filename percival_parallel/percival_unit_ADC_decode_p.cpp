3b27ec6f5f5244346cfdc2c9a4d3b8410c2ade62
qgu <qiushi.gu@diamond.ac.uk>
2015-08-20 17:00:07 +0100

    Put dimension and data validity checks into functions and collated them into one src file.

--- percival_parallel/percival_unit_ADC_decode_p.cpp
+++ percival_parallel/percival_unit_ADC_decode_p.cpp
@@ -8,25 +8,15 @@
 #include "percival_parallel.h"
 #include "percival_exceptions.h"
 #include "tbb/task_scheduler_init.h"
+#include "percival_data_validity_checks.h"
+
 void percival_unit_ADC_decode_pf(
 		const percival_frame<unsigned short int> & input,
 		percival_frame<unsigned short int> & Coarse,
 		percival_frame<unsigned short int> & Fine,
 		percival_frame<unsigned short int> & Gain)
 {
-	if(input.width != Coarse.width || input.height != Coarse.height)
-		throw dataspace_exception("In percival_unit_ADC_decode: coarse_frame and input frame dimensions mismatch.");
-	if(input.width != Fine.width || input.height != Fine.height)
-		throw dataspace_exception("In percival_unit_ADC_decode: fain_frame and input frame dimensions mismatch.");
-	if(input.width != Gain.width || input.height != Gain.height)
-		throw dataspace_exception("In percival_unit_ADC_decode: gain_frame and input frame dimensions mismatch.");
-
-	if(Coarse.data == Fine.data)
-		throw datatype_exception("In percival_unit_ADC_decode: Coarse and Fine frame pointers are identical.");
-	if(Coarse.data == Gain.data)
-		throw datatype_exception("In percival_unit_ADC_decode: Coarse and Gain frame pointers are identical.");
-	if(Gain.data == Fine.data)
-		throw datatype_exception("In percival_unit_ADC_decode: Gain and Fine frame pointers are identical.");
+	percival_unit_ADC_decode_check(input,Coarse,Fine,Gain);
 
 	unsigned int NoOfPixels = input.width * input.height;
 
